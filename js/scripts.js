4411387d8ac7fc468defe12e063db2a98cf99c1a
Isaac Hall and Nathan Nielson <nathan@>
2019-01-23 11:02:25 -0800

    did many things

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+
+//
+// /***************************************************
+// COUNT UP BY USING FOR LOOP
+// *///
+// $(document).ready(function(){
+//   $("form").submit(function(event){
+//     var formOneInput = parseInt($("#formOne").val());
+//     var formTwoInput = parseInt($("#formTwo").val());
+//
+//     for (var currentNumber = formOneInput; currentNumber <= formTwoInput; currentNumber += formOneInput) {
+//
+//     console.log(currentNumber);
+//     }
+//
+//
+//
+//   event.preventDefault();
+//   });
+// });
+// // for (var index = 1; index <= 3; index += 1) {
+// //   alert(index);
+// // }
+
+
+
+
+
+/*******************************************************
+TURN forEach() into for
+*/
+
+//  var languages = ['HTML', 'CSS', 'JavaScript'];
+//
+//
+//
+// for (var language = 0; language <= languages.length-1 ; language += 1 ) {
+//
+//    console.log(languages[language]);
+// };
+
+
+/*************************************************************
+WORD PUZZLE
+*/
+
+
+
+$(document).ready(function(){
+   $("form").submit(function(event){
+     var formOneInput = $("#formOne").val();
+
+     var convert = formOneInput.replace(/a|e|i|o|u|y/g, "-");
+     $('input').hide();
+     alert(convert);
+    event.preventDefault();
+   });
+ });
